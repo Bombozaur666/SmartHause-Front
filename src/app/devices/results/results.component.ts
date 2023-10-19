9030666d8a1ca3226a5b653481e6395c133d52fb
Bombo <bombozaur6@gmail.com>
2023-10-19 19:55:30 +0200

    proper result initialization

--- src/app/devices/results/results.component.ts
+++ src/app/devices/results/results.component.ts
@@ -3,32 +3,37 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Device } from 'src/app/models/device.model';
 import { ResultService } from './results.service';
 import { Results } from 'src/app/models/results.model';
+import { DeviceService } from '../device.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html'
 })
 export class ResultsComponent implements OnInit {
-  @Input() device: Device;
-  @Input() type:string;
+  private device: Device;
+  private id:number;
+
 
   protected resultsTemp: Results[] = [];
   protected error: string|null = null;
   protected isFetching:boolean = false;
   private tab:string|null = null;
 
-  constructor(private http: HttpClient, private res: ResultService) {}
+  constructor(private http: HttpClient, 
+              private res: ResultService,
+              private dev: DeviceService,
+              private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.id = +this.route.snapshot.paramMap.get('id');
+    this.device = this.dev.getDevice(this.id);
     this.onFetch();
   }
 
   onFetch(): void {
     this.isFetching = true;
-    if (this.device.type == '') {
-    this.res.fetchResults(this.device.id, this.device.type)
-      
-    }
+    console.log(this.device);
   };
 
 }
